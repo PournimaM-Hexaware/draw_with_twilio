22737dc5bf071b8e41460f30ec494af938253d3f
Pourab Karchaudhuri <36023@Hexaware.com>
2020-06-16 17:00:16 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,39 @@
+// 'use strict';
+
+// require('dotenv').load();
+
+// const express = require('express');
+// const morgan = require('morgan');
+// const path = require('path');
+// const app = express();
+// const request=require('request')
+// // app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+
+// app.use(express.static(path.resolve(__dirname, '..', 'js')));
+// app.use(express.static(path.resolve(__dirname, '..', 'css')));
+// app.use(express.static(path.resolve(__dirname, '..', 'html')));
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+// // request({
+// //     url: 'https://twilioaccesstokenserver.herokuapp.com/',
+// //     method: 'GET', 
+// //   }, function(error, response, body){
+// //     console.log('error', error);
+// //     console.log('response', response);
+// //     console.log('body', body);
+// //   });
+
+// app.get('/', (req, res) => {
+//     res.redirect('/index.html');
+//   });
+  
+
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
